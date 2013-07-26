0b84cc407fae6eaebeebea16d4cd6b4bf7c98848
cl- <ubuntu@ip-172-31-14-152.us-west-2.compute.internal>
2013-07-26 06:54:23 +0000

    now gets from html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+var htmlFile = fs.readFileSync("index.html");
+var buf = new Buffer(htmlFile, "utf-8");
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString()); 
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
